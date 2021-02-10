bef40e44de7e39f31e237152be7a13a401f2fa3a
yukito-0908 <tourentiancun@gmail.com>
2021-02-10 14:33:10 +0000

    [finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '179f57d061bd3ee69a40c98b6ef906bc817e8b66695f3b0dd2c8d4dee2f7ae05a17d2efee9f0719ec513a1599f9bbe0dc86b163ceef9dd8e47348a3a0e728a64'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  root 'post_images#index'
+  devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
